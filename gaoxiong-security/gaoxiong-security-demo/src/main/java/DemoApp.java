49bf5e1a6e235579c5e422521673a01ee79666df
200506725@qq.com <gaojunchen520>
2018-10-25 00:18:13 +0800

    second commit

---
+++ gaoxiong-security/gaoxiong-security-demo/src/main/java/DemoApp.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gaoxiong
+ * @ClassName DemoApp
+ * @Description TODO
+ * @date 2018/10/23 0:49
+ */
+@SpringBootApplication
+public class DemoApp {
+    public static void main ( String[] args ) {
+        SpringApplication.run(DemoApp.class,args);
+    }
+}
